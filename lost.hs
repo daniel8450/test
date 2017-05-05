a61eea2b9140911a9add79820c8dd1c58cc74faa
Daniel LaBorde <dlabo14@lsu.edu>
2017-05-04 22:58:15 -0500

    Update lost.hs

--- lost.hs
+++ lost.hs
@@ -6,6 +6,7 @@ som n = n * som (n-1)
 toast :: Integer -> Integer
 toast 0 = 0
 toast n = n - toast (n-1)
+  where n > 0
 
 
 
@@ -15,5 +16,5 @@ lost 1 = 1 : []
 lost n = toast n : lost (n-1)
 
 
-main = print (toast 5)
+main = print (lost 5)
 

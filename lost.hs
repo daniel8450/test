1f39f01050db6f6e51aeb62d70857c6c8cfa7a72
Daniel LaBorde <dlabo14@lsu.edu>
2017-05-04 22:28:57 -0500

    Update lost.hs

--- lost.hs
+++ lost.hs
@@ -1,6 +1,11 @@
+sum :: Integer -> Integer
+sum n = n + sum(n-1)
+
+
+
 lost :: Integer -> [Integer]
 --lost 0 = []
-lost n = [x | x <- [n..1]]
+lost n = [x | x <- [sum n]]
 
 
 main = print (lost 5)
